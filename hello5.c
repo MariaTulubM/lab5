6f39f39ac387cc4935863322840993316301c5bd
MariaTulubM <126815717+MariaTulubM@users.noreply.github.com>
2023-12-25 02:29:02 +0200

    Add files via upload

---
+++ hello5.c
@@ -0,0 +1,78 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/ktime.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+
+
+struct time_measurement {
+    struct list_head list;
+    ktime_t start;
+    ktime_t end;
+};
+
+MODULE_AUTHOR("IO-11 Maria Tulub");
+MODULE_DESCRIPTION("Lab5");
+MODULE_LICENSE("GPL");
+
+
+static LIST_HEAD(time_list);
+
+
+static uint count = 1;
+module_param(count, uint, 0444);
+MODULE_PARM_DESC(count, "A count parameter");
+
+
+static int __init my_hello_init(void);
+static void __exit my_hello_exit(void);
+
+static int __init my_hello_init(void) {
+    uint i = 0;
+    struct time_measurement *current_record;
+
+    pr_info("Count: %d\n", count);
+
+    BUG_ON(count == 9);
+    
+    for (i = 0; i < count; i++) {
+        current_record = kmalloc(sizeof(*current_record), GFP_KERNEL);
+
+        if (!current_record)
+            goto error_handle;
+
+        current_record->start = ktime_get();
+        pr_info("Hello, Maria!\n");
+        current_record->end = ktime_get();
+
+        list_add_tail(&current_record->list, &time_list);
+    }
+
+    return 0;
+
+error_handle:
+    pr_err("No memory!\n");
+    my_hello_exit(); 
+    return -ENOMEM;
+}
+
+static void __exit my_hello_exit(void) {
+    struct time_measurement *temp_record, *safe_record;
+
+    list_for_each_entry_safe(temp_record, safe_record, &time_list, list) {
+        pr_info("Execution Time: %lld ns",
+        ktime_to_ns(temp_record->end - temp_record->start));
+
+        list_del(&temp_record->list);
+        kfree(temp_record);
+    }
+
+    BUG_ON(!list_empty(&time_list));
+}
+
+module_init(my_hello_init);
+module_exit(my_hello_exit);
+
